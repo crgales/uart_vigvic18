0de21251b802f016165b3f0f6fc6a3ae2c6d542f
Chuck Gales <cgales@gmail.com>
2023-04-10 11:34:11 -0700

    Refactored code to create WB Agent

---
+++ uart_env/uart_env_pkg.sv
@@ -0,0 +1,9 @@
+package uart_env_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  import wb_agt_pkg::*;
+  `include "uart_env_config.sv"
+  `include "uart_scoreboard.sv"
+  `include "uart_env.sv"
+endpackage
--- wb_agt/uart_seqr.sv
+++
@@ -1,8 +0,0 @@
-class uart_sequencer extends uvm_sequencer#(uart_xtn);
-	`uvm_component_utils(uart_sequencer)
-
-	function new(string name="uart_sequencer",uvm_component parent);
-		super.new(name,parent);
-	endfunction
-endclass
-
